e54d21d513c81133734be1ed1474db2002ba17f3
velvet-jedi <pranjalruhela2811@gmail.com>
2024-08-31 14:32:57 +0530

    get all contacts

--- src/contacts.js
+++ src/contacts.js
@@ -1,5 +1,28 @@
-const localforage = require("localforage");
+import localforage from "localforage";
+import { matchSorter } from "match-sorter";
+import sortBy from "sort-by";
 
+
+export async function getContacts(query){
+    await fakeNetwork(`getContacts:${query}`);
+    let contacts = await localforage.getItem('contacts');
+    if(!contacts) contacts = [];
+    if(query) {
+        contacts = matchSorter(contacts, query, {keys: ['first', 'last']});
+    }
+    return contacts.sort(sortBy("last", "createdAt"));
+}
+
+
+export async function createContact() {
+    await fakeNetwork();
+    let id = Math.random().toString(36).substring(2, 9);
+    let contact = {id, createdAt: Date.now()};
+    let contacts = await getContacts(); 
+    contacts.unshift(contact); // adds the new contact to the beginning of the contacts array.
+    await set(contacts);
+    return contact;
+}
 
 export async function getContact(id) {
     await fakeNetwork(`contact:${id}`);
