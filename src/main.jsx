98b71a366cfc2280012640699d32edecaa01a3c8
velvet-jedi <pranjalruhela2811@gmail.com>
2024-08-31 21:26:25 +0530

    useLoaderData() to create contact

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css'
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
 import ErrorPage from './routes/ErrorPage';
 import Contact from './routes/Contact';
+import { create } from './routes/root';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root/>,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: create,
     children: [
       {
         path: 'contacts/:contactId',
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,20 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link, useLoaderData, Form } from "react-router-dom"
+import { getContacts, createContact } from "../contacts"
+
+export async function loader() {
+    const contacts = await getContacts();
+    return {contacts}; // return as a single-property object
+}
+
+export async function create() {
+    const contact = await createContact();
+    return {contact};
+}
+
+
 
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
         <>
             <div id="sidebar">
@@ -24,15 +38,34 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                     </div>
                     <nav>
-                        <ul>
-                            <li><Link to={`/contacts/1`}>Delhi</Link></li>
-                            <li><Link to={`/contacts/2`}>Sardar</Link></li>
-                        </ul>
+                    {contacts.length ? (
+                            <ul>
+                                {contacts.map((contact) => (
+                                    <li key={contact.id}>
+                                        <Link to={`contacts/${contact.id}`}>
+                                            {contact.first || contact.last ? (
+                                                <>
+                                                    {contact.first} {contact.last} 
+                                                </>
+                                            ) : (
+                                                <i>No Name</i>
+                                            )}{" "}
+                                            {contact.favorite && <span>★</span>}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>
+                                <i>No contacts</i>
+                            </p>
+                        )}
+                        
                     </nav>
                 </div>
                 <div id="detail">
